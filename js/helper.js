df8c0dd3cd62d84cf90fcda5cd937b061590a295
Mark Chen <markchen555@gmail.com>
2017-03-15 00:31:11 -0700

    Doc: Update layout and color

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,8 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span class="grey-text">%data%</span><hr>';
+var HTMLheaderRole = '<span class="orange-text">%data%</span>';
+var HTMLbioPic = '<img src="%data%" class="biopic"><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">Mobile:</span><span class="white-text">%data%</span></li>';
@@ -23,14 +24,14 @@ var HTMLgithub = '<li class="flex-item"><span class="orange-text">Github:</span>
 var HTMLblog = '<li class="flex-item"><span class="orange-text">Blog:</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">Location:</span><span class="white-text">%data%</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
+// var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkEmployer = '<a>%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
